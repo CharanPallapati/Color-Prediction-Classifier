47492ffdc098eaf5e31e63e3a2fd70c0df3e96d2
charan sai kumar <pallapaticharansaikumar@gmail.com>
2024-12-09 13:47:50 +0530

    Update Backend.py

--- Backend.py
+++ Backend.py
@@ -1,7 +1,21 @@
 import pickle
-from flask import Flask,render_template,url_for,request,redirect
+from flask import Flask , render_template, url_for, request, redirect
 import cv2
+import os
+ 
 app=Flask(__name__)
+app.config['upload_folder']="uploads/"
+
+def p(im):
+   i=cv2.imread(im)
+   if i is not None:
+      i=cv2.resize(i,(500,500),interpolation=cv2.INTER_LINEAR)
+      img=i.flatten()
+      with open("model",'rb') as f:
+          mode=pickle.load(f)
+      im=mode.predict([img])
+      return im
+   return None
  
 @app.route('/',methods=['GET','POST'])
 def a():
@@ -11,15 +25,12 @@ def a():
 
 @app.route('/predict',methods=['POST']) 
 def predict():
-   img=request.form['image']
-   i=cv2.imread(img+'.jpg')
-   if i is not None:
-      i=cv2.resize(i,(500,500),interpolation=cv2.INTER_LINEAR)
-      img=i.flatten()
-      with open("model",'rb') as f:
-          mode=pickle.load(f)
-      im=mode.predict([img])
-      return render_template("prediction.html",post=im)
+   img=request.files['image']  
+   im=os.path.join(app.config['upload_folder'],'upload.jpg')
+   img.save(im)
+   im=p(im)
+   if im!=None:
+      return render_template("result.html",post=im)
    return redirect('/')  
 if __name__=="__main__":
    app.run(debug=True)
